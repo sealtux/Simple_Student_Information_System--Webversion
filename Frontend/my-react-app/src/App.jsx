cfa8338fc1bb0467b7850d7cbf22ca598ee1b6bb
Jazrel Xandrei Quinlob <sealtux@gmail.com>
2025-09-07 18:00:40 +0800

    startup tools

---
+++ Frontend/my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Test from './Test';
+import "./assets/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className='header'>Hello world</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
