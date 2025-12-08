7d60d4b15a8f704851c426c5d5f10af2f5f9f6ce
Jazrel Xandrei Quinlob <sealtux@gmail.com>
2025-12-08 10:06:59 +0800

    fixed bug

--- app/views/src/Pages/student/student.jsx
+++ app/views/src/Pages/student/student.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { supabase } from "../../../services/supabase";
+import { supabase } from "../../../../services/supabase";
 
 import "../../assets/styles/student.css";
 import editIcon from "../../assets/images/edit.png";
